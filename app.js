589c5a8c5c7cc7e3cb788ab031e4bbbc69d7de61
pthomson88 <62576469+pthomson88@users.noreply.github.com>
2021-05-21 17:04:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var message = 'Hello, World!';
-// create a new heading 1 element
-var heading = document.createElement('h1');
-heading.textContent = message;
-// add the heading the document
-document.body.appendChild(heading);
+// next up a map
+var mapUrl = 'https://www.google.com/maps/d/embed?mid=1JsZ0qoeoCehcQnX9fXjmDTCpxYl9QLdc&ll=55.87827418515526%2C-3.1336741860674344&z=12';
+// create the embedded map as an iframe
+var iframe = document.createElement('iframe');
+iframe.src = mapUrl;
+iframe.width = "600";
+iframe.height = "450";
+iframe.allowFullscreen = false;
+// add the iframe to the document
+document.body.appendChild(iframe);
